7c05e66843d85c8cef013853e472a7d13dbbf9f0
dr-ni <uwe_niethammer@web.de>
2024-10-22 02:06:47 +0200

    Updated some bugs

--- sphinx/voxgen/voicecommands.sh
+++ sphinx/voxgen/voicecommands.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-for voicecmdfile in "$(dirname $0)/voicecommands.conf" /usr/local/share/voxgen/voicecommands.conf
-do 
-if [ -f "$voicecomdfile" ] ; then
-	echo using $voicecomdfile
-fi
-done
-
-
+voicecmdfile='/home/uwe/robotino/sphinx/voxgen/voicecommands.conf'
+audiopath='/var/www/html/audio'
 #define shortcuts
-speak='espeak -v mb-de6 -s 150'
-ans=$1
-mute='mutepulse.sh'
-unmute='umutepulse.sh'
-
-
-cmd=$(awk -F "\t" --assign voicestr="$1" 'BEGIN{}/!^#/{}{if (voicestr == $1){print $2}}' $voicecmdfile)
-
-#if [ -z "$cmd" ] ; then
-#	$speak "nicht verstanden"
-#	exit 1
-#fi
-eval $mute
-eval $cmd
-eval $unmute
+# speak='espeak -v mb-de6 -s 150'
+speak='/usr/local/bin/espeak_speak.sh'
+mute='/home/uwe/pilotx/mic_mute.sh'
+umute='/home/uwe/pilotx/mic_init.sh'
+string=$(echo "$1")
+echo "string=$string"
+mute='/home/uwe/pilotx/mic_mute.sh'
+unmute='/home/uwe/pilotx/mic_init.sh'
+echo
+#echo awk -F "\t" --assign voicestr="$string" 'BEGIN{}/!^#/{}{if (voicestr == $1){print $2}}' $voicecmdfile
+cmd=$(awk -F "\t" --assign voicestr="$string" 'BEGIN{}/!^#/{}{if (voicestr == $1){print $2}}' $voicecmdfile)
+echo "cmd=$cmd"
+if [ -z "$cmd" ] ; then
+#	echo "=> nicht verstanden"
+notify-send -u normal -t 2000 "Unknown" "command"
+	exit 1
+else
+[[ -z "$string" ]] || notify-send -u normal -t 2000 "Command:" "$string"
+        eval $mute
+        echo $cmd
+        eval $cmd
+        eval $unmute
+        echo
+fi
